ee337468df26e329b43971c1a364e560a5990243
nikhil1996b <30387741+Nikhil1996B@users.noreply.github.com>
2024-01-14 06:46:32 +0000

    Update 6 files

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,2 +1,52 @@
 import { useState, useEffect } from "react";
-import {} from "@mui/material";
+import { TextField, Box } from "@mui/material";
+import { useSelector, useDispatch } from "react-redux";
+import { getMovies } from "../redux/feature/movieSlice";
+
+const Search = () => {
+  const [movieName, setMovieName] = useState("spider");
+  const dispatch = useDispatch();
+  const classes = useStyles();
+  useEffect(() => {
+    dispatch(getMovies(movieName));
+  }, [movieName]);
+
+  return (
+    <>
+      <Box
+        as="h2"
+        sx={{
+          letterSpacing: "0.1rem",
+          lineHeight: "1.25",
+          marginBottom: "0.25rem",
+          fontSize: "2rem",
+        }}
+      >
+        Movie Search App
+      </Box>
+      <Box
+        as="form"
+        className={{
+          width: "90vw",
+          maxWidth: "1170px",
+          margin: "0 auto",
+          marginTop: "3rem",
+          marginBottom: "3rem",
+        }}
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+      >
+        <TextField
+          type="text"
+          fullWidth
+          value={movieName}
+          sx={{ m: 1, width: "55ch" }}
+          onChange={(e) => setMovieName(e.target.value)}
+        />
+      </Box>
+    </>
+  );
+};
+
+export default Search;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Search from "../components/Search";
 
 export default Home = () => {
   return (
     <>
       {/* Search */}
+      <Search />
       {/* Movie list */}
     </>
   );
